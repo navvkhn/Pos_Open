ff434e67c2d97b4aabdfee89d882ed760ad1f41c
navvkhn <69992995+navvkhn@users.noreply.github.com>
2026-01-08 22:06:17 +0530

    Update db.py

--- db.py
+++ db.py
@@ -9,7 +9,12 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(
     DATABASE_URL,
-    connect_args={"sslmode": "require"}
+    connect_args={
+        "sslmode": "require",
+        "options": "-c statement_timeout=5000"
+    },
+    pool_pre_ping=True
 )
+
 SessionLocal = sessionmaker(bind=engine)
 Base = declarative_base()
