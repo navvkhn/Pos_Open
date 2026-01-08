adf1f3b03bcbd5148d0baac3618d692744632d19
navvkhn <69992995+navvkhn@users.noreply.github.com>
2026-01-09 01:43:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,33 +1,23 @@
 import streamlit as st
 from auth import login
 from admin.products import products
-from admin.discounts import discounts
 from admin.reports import reports
-from pos.billing import billing
-from customer.menu import customer_menu
-from auth import login
 
 st.set_page_config(page_title="Superscale POS", layout="wide")
 
 if "user" not in st.session_state:
     login()
 else:
-    tenant = st.session_state.user["tenant"]
     tenant_id = st.session_state.user["tenant_id"]
+    st.sidebar.title(st.session_state.user["tenant"])
 
-    st.sidebar.title(f"🍽 {tenant}")
     page = st.sidebar.selectbox(
         "Menu",
-        ["POS Billing", "Products", "Discounts", "Reports", "Customer Menu"]
+        ["Products", "Reports"]
     )
 
-    if page == "POS Billing":
-        billing(tenant_id)
     if page == "Products":
         products(tenant_id)
-    if page == "Discounts":
-        discounts(tenant_id)
+
     if page == "Reports":
         reports(tenant_id)
-    if page == "Customer Menu":
-        customer_menu(tenant_id)
