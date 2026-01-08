a4584e343e47eccbccc5202b92f70c6a2ee1874d
navvkhn <69992995+navvkhn@users.noreply.github.com>
2026-01-08 21:49:33 +0530

    Update db.py

--- db.py
+++ db.py
@@ -7,6 +7,9 @@ load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"sslmode": "require"}
+)
 SessionLocal = sessionmaker(bind=engine)
 Base = declarative_base()
