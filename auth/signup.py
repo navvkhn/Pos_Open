8dcce5f322b60101fb2e517e4ee6301d8b4bdab5
navvkhn <69992995+navvkhn@users.noreply.github.com>
2026-01-08 21:40:53 +0530

    Update signup.py

--- auth/signup.py
+++ auth/signup.py
@@ -6,10 +6,10 @@ from passlib.hash import pbkdf2_sha256
 def signup():
     st.title("📝 Restaurant Signup")
 
-restaurant = st.text_input("Restaurant Name", key="signup_restaurant")
-username = st.text_input("Admin Username", key="signup_username")
-password = st.text_input("Password", type="password", key="signup_password")
-confirm = st.text_input("Confirm Password", type="password", key="signup_confirm")
+        restaurant = st.text_input("Restaurant Name", key="signup_restaurant")
+        username = st.text_input("Admin Username", key="signup_username")
+        password = st.text_input("Password", type="password", key="signup_password")
+        confirm = st.text_input("Confirm Password", type="password", key="signup_confirm")
 
 
     if st.button("Create Account"):
